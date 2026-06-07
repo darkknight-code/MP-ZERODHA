5aaf3ca53475f8ba9f92376094e5612af9920fb3
darkknight-code <alkeshshukla60@gmail.com>
2026-06-07 12:32:28 +0530

    Fix eslint warnings

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { href } from 'react-router-dom';
 
 function Hero() {
     return ( 
@@ -7,7 +8,7 @@ function Hero() {
                 <img src='./media/homeHero.png' alt='Hero Img' className='mb-5'></img>
                 <h1 className='mt-5'>Invest in everthing</h1>
                 <p style={{fontWeight:"450 px"}}>Online platform to invest in stocks,derivates,mutal funds,and more</p>
-                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}} href="/signup">Signup Now</button>
             </div>
 
         </div>
