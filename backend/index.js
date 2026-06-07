cff59ff061f0f0c2d218e826fcc4e3965b8fc2ee
darkknight-code <alkeshshukla60@gmail.com>
2026-06-07 10:34:38 +0530

    alowing links to cors for frontend and dashboard

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 3002;
 
 
 app.use(cors({
-  origin: ["http://localhost:3001","http://localhost:3000"],   
+  origin: ["http://localhost:3001","http://localhost:3000","https://your-frontend.vercel.app",
+  "https://your-dashboard.vercel.app"],   
   credentials: true,
 }));
 
